628847c1f4a4e10a883be8d21aa987f574f3af33
Simon Brooke <simon@journeyman.cc>
2020-01-27 10:41:23 +0000

    Updated project to hopefully do release

--- project.clj
+++ project.clj
@@ -1,11 +1,27 @@
 (defproject csv2edn "0.1.0-SNAPSHOT"
   :description "Simple command line utility to convert CSV files to EDN"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.csv "0.1.4"]
                  [org.clojure/tools.cli "0.4.2"]]
   :main ^:skip-aot csv2edn.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :plugins [[lein-codox "0.10.7-multilang"]
+            [lein-kibit "0.1.6"]
+            [lein-release "1.0.5"]]
+  :profiles {:uberjar {:aot :all}}
+
+  :lein-release {:deploy-via :clojars}
+
+  ;; `lein release` doesn't play nice with `git flow release`. Run `lein release` in the
+  ;; `develop` branch, then reset the `master` branch to the release tag.
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["clean"]
+                  ["uberjar"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]])
